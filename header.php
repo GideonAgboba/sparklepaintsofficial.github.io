30447ba44ddf45880aaab8dc6c341f76b2c083a5
Gideon <33206022+GideonAgboba@users.noreply.github.com>
2018-02-09 08:00:48 -0800

    Update header.php

--- header.php
+++ header.php
@@ -6,12 +6,9 @@
 	<link rel="stylesheet" type="text/css" href="css/carousel.css">
 	 
 	<!-- JQuery -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 	<script src="css/bootstrap.min.js"></script>
 	<script src="sparkle-min.css"></script>
 	 
-	<!-- Latest compiled and minified JavaScript -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
  
 </head>
  
